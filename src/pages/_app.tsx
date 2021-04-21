98e21a400ec8c2bb0fec4b6aa0534d2da993e320
gusbdev <gusbdev@gmail.com>
2021-04-21 13:37:11 -0300

    Third day

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import '../styles/global.scss';
+
 import { Header } from '../components/Header';
-import '../styles/global.scss'
-import styles from '../styles/app.module.scss';
 import { Player } from '../components/Player';
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
